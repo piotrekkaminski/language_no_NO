e1a107a79e7dbfc590a1737504d103bca4ff5942
Piotr Kaminski <magentopeter@gmail.com>
2017-08-25 15:03:44 -0500

    Translation autoupdate from Crowdin for no_NO - 2017-08-25 20:03:44 branch Head

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magentotwotranslations_no_no',
+    __DIR__
+);
